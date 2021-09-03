7e32fa66e6cc7eff8d273265eee21f03d83c9137
Dimple Verma <73439066+Dimple79@users.noreply.github.com>
2021-09-04 01:19:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//javascript for navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//javascript for responsive navigation sidebar menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a")
+
+menuBtn.addEventListener("click",  () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener("click", () => {
+    menuBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  });
+});
+
+
